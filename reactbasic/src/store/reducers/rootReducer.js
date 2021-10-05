9b887362415a3b9fb3e8ab2c4b306ce9c7901ea4
thaibanghoang <thaibang1983hn@gmail.com>
2021-10-05 23:55:53 +0200

    end

--- reactbasic/src/store/reducers/rootReducer.js
+++ reactbasic/src/store/reducers/rootReducer.js
@@ -6,7 +6,18 @@ const initState = {
   ],
 };
 const rootReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "DELETE_USER":
+      let users = state.users;
+      users = users.filter((item) => item.id !== action.payload.id);
+      return {
+        ...state,
+        users,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
--- reactbasic/src/views/Components/Home.js
+++ reactbasic/src/views/Components/Home.js
@@ -11,12 +11,30 @@ class Home extends React.Component {
     // }, 3000);
   }
 
+  handleDeleteUser = (users) => {
+    this.props.deleteUserRedux(users);
+  };
+
   render() {
+    let listUsers = this.props.dataRedux;
+
     return (
       <div>
         <span>Home Page</span>
         <p>React Example Basic</p>
         <img src={caphesang} alt="caphe" />
+        <div>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name}
+                  <span onClick={() => this.handleDeleteUser(item)}>x</span>
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
@@ -25,7 +43,14 @@ class Home extends React.Component {
 // export default withRouter(Home);
 
 const mapStateToProps = (state) => {
-  return { dataRedux: state.user };
+  return { dataRedux: state.users };
 };
 
-export default connect(mapStateToProps)(Color(Home));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteUserRedux: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
